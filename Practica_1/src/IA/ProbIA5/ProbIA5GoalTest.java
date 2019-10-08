464fae84390fa1c40c5ebf871786663ba043573f
Marc <marc.monfort@gmail.com>
2019-10-08 15:54:03 +0200

    goalTest done

--- Practica_1/src/IA/ProbIA5/ProbIA5GoalTest.java
+++ Practica_1/src/IA/ProbIA5/ProbIA5GoalTest.java
@@ -2,13 +2,10 @@ package IA.ProbIA5;
 
 import aima.search.framework.GoalTest;
 
-/**
- * Created by bejar on 17/01/17.
- */
+
 public class ProbIA5GoalTest implements GoalTest {
 
-    public boolean isGoalState(Object state){
-
-        return((ProbIA5Board) state).is_goal();
-    }
+    public boolean isGoalState(Object aState) {
+    return(false);
+  }
 }
